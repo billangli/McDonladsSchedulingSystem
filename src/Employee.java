dd6fa0c11af2e2a709bd6d487f83a3513aa41b09
Bill Li <libill12@hotmail.com>
2016-10-31 12:22:50 -0400

    I added the three variables

--- src/Employee.java
+++ src/Employee.java
@@ -11,6 +11,9 @@ class Employee {
     private String fullName;
     private String address;
     private String employeeNumber;
+    private boolean[][] hoursAvailable = new boolean[7][24];
+    private boolean[][] hoursWorking = new boolean[7][24];
+    private int totalHours = 0;
 
     public String getFullName() {
         return fullName;
@@ -34,5 +37,29 @@ class Employee {
 
     public void setEmployeeNumber(String employeeNumber) {
         this.employeeNumber = employeeNumber;
+    }
+
+    public boolean[][] getHoursAvailable() {
+        return hoursAvailable;
+    }
+
+    public void setHoursAvailable(boolean[][] hoursAvailable) {
+        this.hoursAvailable = hoursAvailable;
+    }
+
+    public boolean[][] getHoursWorking() {
+        return hoursWorking;
+    }
+
+    public void setHoursWorking(boolean[][] hoursWorking) {
+        this.hoursWorking = hoursWorking;
+    }
+
+    public int getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(int totalHours) {
+        this.totalHours = totalHours;
     }
 }
